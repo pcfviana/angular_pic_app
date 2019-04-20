da475005c2d9d0a30932f001a973602232393048
Paulo Viana <pauloviana@MacBook-Pro-de-Paulo.local>
2019-04-20 19:57:28 -0300

    Criação do componente Photo-List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { PhotoService } from './photos/photo/photo.service';
 import { Photo } from './photos/photo/photo';
@@ -9,15 +9,7 @@ import { Photo } from './photos/photo/photo';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'app';
-  photos: Photo[] = [];
-
-  constructor(private photoService: PhotoService){
-    
-  }
-
-  ngOnInit(): void{
-    this.photoService.listFromUser('viana').subscribe(photos => this.photos = photos);
-  }
+  
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
-
-import { PhotoComponent } from './photo/photo.component';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+
+
 @NgModule({
-    declarations: [ PhotoComponent ], //privado
-    exports: [ PhotoComponent ], // público
-    imports: [ HttpClientModule ]
+    declarations: [ 
+        PhotoComponent,
+         PhotoListComponent 
+        ], //privado
+    //exports: [ PhotoComponent ], // público
+    imports: [ 
+        HttpClientModule,
+        CommonModule
+     ]
 })
 export class PhotosModule{
 
