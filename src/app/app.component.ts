9de9c4892268dbc4e1d077aa249839bf9650710c
Paulo Viana <pauloviana@MacBook-Pro-de-Paulo.local>
2019-04-19 22:50:34 -0300

    Adequando o template gerado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  description = "f-14";
+  url = "https://hlj.com/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/t/a/tam60782_6.png";
+  
 }
