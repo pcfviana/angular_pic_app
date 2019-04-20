34556c24c33200e654fca6059b93a2941f3a418a
Paulo Viana <pauloviana@MacBook-Pro-de-Paulo.local>
2019-04-20 20:36:06 -0300

    Tratando rota inválida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './app.rounting.module';
+import { ErrorsModule } from './errors/errors.module';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { AppRoutingModule } from './app.rounting.module';
   imports: [
     BrowserModule,
     PhotosModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
