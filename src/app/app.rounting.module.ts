ef7893a20c13effbe4c63523b09136b1ac5b9692
Paulo Viana <pauloviana@MacBook-Pro-de-Paulo.local>
2019-04-20 20:49:31 -0300

    Melhorando o roteamento

--- src/app/app.rounting.module.ts
+++ src/app/app.rounting.module.ts
@@ -8,6 +8,7 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 const routes: Routes = 
 [
     { path: 'photos', component: PhotoListComponent },
+    { path: 'photos/:userName', component: PhotoListComponent },
     { path: 'p/add', component: PhotoFormComponent },
     { path: '**', component: NotFoundComponent }
 ];
--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-photo-list',
@@ -12,12 +13,18 @@ export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
 
-  constructor(private photoService: PhotoService){
-    
-  }
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+    ){ }
 
   ngOnInit(): void{
-    this.photoService.listFromUser('viana').subscribe(photos => this.photos = photos);
+
+    const userName = this.activatedRoute.snapshot.params.userName;
+
+    console.log(userName);
+
+    this.photoService.listFromUser(userName).subscribe(photos => this.photos = photos);
   }
 
 }
