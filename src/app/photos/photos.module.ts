92a183be89dd855c268455d85d72a8ea5519ced7
Paulo Viana <pauloviana@MacBook-Pro-de-Paulo.local>
2019-04-21 14:43:05 -0300

    Adicionando comp. photos e melhorias no photo-list

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,13 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 
 @NgModule({
     declarations: [ 
         PhotoComponent,
          PhotoListComponent,
-         PhotoFormComponent 
+         PhotoFormComponent,
+         PhotosComponent 
         ], //privado
     //exports: [ PhotoComponent ], // público
     imports: [ 
