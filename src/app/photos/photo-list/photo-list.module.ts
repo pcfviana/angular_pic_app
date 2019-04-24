09c974783e3ac0b52dc124cc47f882544c35d303
Paulo César Viana <paulo.cesar@implantainformatica.com.br>
2019-04-24 18:09:26 -0300

    Criação e uso do componente card

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -6,6 +6,7 @@ import { PhotosComponent } from './photos/photos.component';
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { FilterByDescription } from './filter-by-description.pipe';
 import { PhotoModule } from '../photo/photo.module';
+import { CardModule } from 'src/app/shared/card/card.module';
 
 @NgModule({
     declarations: [
@@ -17,7 +18,8 @@ import { PhotoModule } from '../photo/photo.module';
 
     imports: [ 
         CommonModule,
-         PhotoModule 
+         PhotoModule ,
+         CardModule
     ]
 })
 export class PhotoListModule
