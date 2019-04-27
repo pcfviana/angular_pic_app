dbce9f10cbd249a5046f71a978e20d61ee5cfc92
Paulo Viana <pauloviana@MacBook-Pro-de-Paulo.local>
2019-04-26 23:56:48 -0300

    Validação dos campos de login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from "@angular/core";
 import { SignInComponent } from "./signin/signin.component";
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { VMessageModule } from '../shared/vmessage/vmessage.module';
 
 @NgModule({
-    declarations: [ SignInComponent ]
+    declarations: [ SignInComponent ],
+    imports: [ 
+        CommonModule,
+         ReactiveFormsModule,
+         VMessageModule
+    ]
 })
 export class HomeModule{}
---
+++ src/app/shared/vmessage/vmessage.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { VMessageComponent } from './vmessage.compoent';
+
+@NgModule({
+    declarations: [ VMessageComponent ],
+    exports: [ VMessageComponent ]
+})
+export class VMessageModule{}
