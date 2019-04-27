4462dd568f395126b0d5da6fdc2513b87072c79e
Paulo Viana <pauloviana@MacBook-Pro-de-Paulo.local>
2019-04-27 00:27:10 -0300

    Iniciando a autenticação (login)

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
     templateUrl : './signin.component.html'
@@ -7,7 +8,8 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class SignInComponent implements OnInit  {
     loginForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder){
+    constructor(private formBuilder: FormBuilder,
+                private authService: AuthService){
 
     }
 
@@ -16,5 +18,21 @@ export class SignInComponent implements OnInit  {
             userName: ['', Validators.required],
             password: ['', Validators.required]
         });
+    }
+
+    login()
+    {
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+        
+        this
+        .authService
+        .authenticate(userName, password)
+        .subscribe(
+            () => console.log('autenticado'),
+            err => {
+                console.log(err);
+                this.loginForm.reset();
+            } );
     }
 }
