9b254c3fe52b577078c4109e1a7bc9a8d389875f
Paulo Viana <pauloviana@MacBook-Pro-de-Paulo.local>
2019-04-28 13:10:57 -0300

    Armazenamento do token

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
+import { TokenService } from '../token/token.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -9,15 +10,14 @@ const API_URL = 'http://localhost:3000';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private tokenService: TokenService) { }
 
   authenticate(userName: string, password:string){
     return this.http.post(API_URL + '/user/login',{ userName, password }, { observe: 'response' })
     .pipe(tap(res => {
       const authToken = res.headers.get('x-access-token');
-      console.log(
-        `User ${userName} 
-        authenticated with token ${authToken}` );
+      this.tokenService.setToken(authToken);
     }));
   }
 }
