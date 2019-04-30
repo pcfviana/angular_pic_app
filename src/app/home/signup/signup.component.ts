433e979580658cbfb764010ac5ad727017f4d880
Paulo Viana <pauloviana@MacBook-Pro-de-Paulo.local>
2019-04-29 22:20:54 -0300

    signup form e validação do formulário

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,8 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './signup.component.html'
 })
-export class SignUpComponent{
-    
+export class SignUpComponent implements OnInit {
+   
+ 
+    signupForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder){}
+
+     ngOnInit(): void {
+        this.signupForm = this.formBuilder.group({
+            email: ['', 
+                [
+                    Validators.required,
+                    Validators.email
+                ]
+            ],
+            userName: ['', 
+                [
+                    Validators.required,
+                    //Validators.pattern('/ˆ[a-z0-9_\]+$/'),
+                    Validators.minLength(2),
+                    Validators.maxLength(30)
+                ]
+            ],
+            fullName: ['', 
+                [
+                    Validators.required,
+                    Validators.minLength(2),
+                    Validators.maxLength(40)
+                ]
+            ],
+            password: ['', 
+                [
+                    Validators.required,
+                    Validators.minLength(2),
+                    Validators.maxLength(14)
+                ]
+            ]
+
+        });
+    }
 }
