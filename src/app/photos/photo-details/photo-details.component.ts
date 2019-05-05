fd77f03d420698eee8d919b4fa3b5639b9654b49
Paulo Viana <pauloviana@MacBook-Pro-de-Paulo.local>
2019-05-05 14:31:34 -0300

    Recuperando e exibindo a foto selecionada.

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PhotoService } from '../photo/photo.service';
+import { Photo } from '../photo/photo';
+import { Observable } from 'rxjs';
 
 @Component({
     
-    templateUrl: './photo-details.component.html'
+    templateUrl: './photo-details.component.html',
+    styleUrls: ['./photo-details.component.css']
 })
 export class PhotoDetailsComponent implements OnInit {
     
-
-    constructor(private router: ActivatedRoute){
-
-    }
+    photo$: Observable<Photo>;
+    constructor(private router: ActivatedRoute,
+                private photoService: PhotoService
+    ){}
 
     ngOnInit(): void {
-        const id = this.router.snapshot.params.photoId;
+         this.photo$ = this.photoService.findById(this.router.snapshot.params.photoId);
     }
 
 }
